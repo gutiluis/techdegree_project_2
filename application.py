8f27967a28989dfdbfab4602fba85a0209f59195
luis <gutierrez.luis@icloud.com>
2024-11-19 17:02:16 -0800

    Add files via upload

---
+++ application.py
@@ -0,0 +1,23 @@
+#list of teams
+#list of players
+# cleaning up player data
+# total number of players
+# equal number. 2 teams
+# dunder main, html, javascript, 
+# flake8, debugger
+# python3 applications will not run the content of the dm.py file
+import dm
+from constants import players
+from constants import teams
+
+
+
+
+# all CAPS variable that cannot be changed
+def clean_data():
+	copy.deepcopy(height)
+	copy.deepcopy(experience=True or False)
+	players
+	teams
+
+# lists with nested dictionaries
